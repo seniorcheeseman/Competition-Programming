4a525f3fa9c05ceb4b581b922b70cedbf413253c
seniorcheeseman <artli_cheese@yahoo.com>
2015-09-29 19:59:00 -0500

    subsequence

---
+++ ProgrammingProblems/src/uvaProblems/Uva1121.java
@@ -0,0 +1,46 @@
+package uvaProblems;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Uva1121 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		while(input.hasNext())
+		{
+			long n = input.nextLong();
+			long s = input.nextLong();
+			long[] nums = new long[(int)n];
+			int minimum = Integer.MAX_VALUE;
+			int hi = 0,low = 0;
+			for(long x=0; x<n;x++)
+			{
+				nums[(int)x] = input.nextLong();
+			}
+			long sum = nums[0];
+			while (hi < n) {
+				if (sum >= s) {
+					int temp = hi - low + 1;
+					if (temp < minimum)
+						minimum = temp;
+				}
+				if (sum >= s && low < hi) {
+					sum -= nums[low];
+					low++;
+				} else if (sum < s) {
+					hi++;
+					if (hi < n)
+						sum += nums[hi];
+				}
+			}
+			if(minimum==Integer.MAX_VALUE)minimum =0;
+			System.out.println(minimum);
+		}
+	}
+
+}
